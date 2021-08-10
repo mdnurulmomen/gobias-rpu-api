4f632a0c5dfad73d675f2b8879296841491c0629
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2021-08-10 12:13:13 +0600

    update web

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/store-office', [App\Http\Controllers\OfficeController::class, 'storeOffice']);
+Route::post('/store-employee', [App\Http\Controllers\EmployeeRecordController::class, 'storeEmployee']);
+Route::post('/update-employee', [App\Http\Controllers\EmployeeRecordController::class, 'updateEmployee']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
-Route::post('/store-office', [App\Http\Controllers\OfficeController::class, 'storeOffice']);
-Route::post('/store-employee', [App\Http\Controllers\EmployeeRecordController::class, 'storeEmployee']);
-Route::post('/update-employee', [App\Http\Controllers\EmployeeRecordController::class, 'updateEmployee']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
