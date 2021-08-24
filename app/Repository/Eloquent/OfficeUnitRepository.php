ea3fb77d9dd045b6f7ee4d8e2ccce88cffe24a8e
Robin <faisalrobin22@gmail.com>
2021-08-24 15:15:42 +0600

    office unit list api update

--- app/Repository/Eloquent/OfficeUnitRepository.php
+++ app/Repository/Eloquent/OfficeUnitRepository.php
@@ -45,10 +45,59 @@ class OfficeUnitRepository implements BaseRepositoryInterface
 
     public function list(Request $request)
     {
-        if ($request->per_page && !$request->all) {
-            return OfficeUnit::paginate($request->per_page);
+        $office_ministry_id = $request->office_ministry_id;
+        $office_unit_category = $request->office_unit_category;
+        $parent_unit_id = $request->parent_unit_id;
+        $office_layer_id = $request->office_layer_id;
+        $office_id = $request->office_id;
+        $unit_name_bng = $request->unit_name_bng;
+        $unit_name_eng = $request->unit_name_eng;
+        $created_by = $request->created_by;
+        $modified_by = $request->modified_by;
+
+        $query = OfficeUnit::query();
+
+
+        $query->when($unit_name_bng, function ($q, $unit_name_bng) {
+            return $q->where('unit_name_bng', 'LIKE',"%{$unit_name_bng}%");
+        });
+
+        $query->when($unit_name_eng, function ($q, $unit_name_eng) {
+            return $q->where('unit_name_eng', 'LIKE',"%{$unit_name_eng}%");
+        });
+
+        $query->when($office_ministry_id, function ($q, $office_ministry_id) {
+            return $q->where('office_ministry_id', $office_ministry_id);
+        });
+
+        $query->when($office_unit_category, function ($q, $office_unit_category) {
+            return $q->where('office_unit_category', $office_unit_category);
+        });
+
+        $query->when($parent_unit_id, function ($q, $parent_unit_id) {
+            return $q->where('parent_unit_id', $parent_unit_id);
+        });
+
+        $query->when($office_layer_id, function ($q, $office_layer_id) {
+            return $q->where('office_layer_id', $office_layer_id);
+        });
+
+        $query->when($office_id, function ($q, $office_id) {
+            return $q->where('office_id', $office_id);
+        });
+
+        $query->when($created_by, function ($q, $created_by) {
+            return $q->where('created_by', $created_by);
+        });
+
+        $query->when($modified_by, function ($q, $modified_by) {
+            return $q->where('modified_by', $modified_by);
+        });
+
+        if ($request->per_page) {
+            return $query->paginate($request->per_page);
         } else {
-            return OfficeUnit::all();
+            return $query->get()->toArray();
         }
     }
 
