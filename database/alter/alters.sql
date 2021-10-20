7e3d036d4a63019b2f35c487a72bcab970e1264a
theeMahmud <mahmudrajucse@gmail.com>
2021-10-21 01:32:51 +0600

    alter

---
+++ database/alter/alters.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `audit_queries` DROP `ministry_id`, DROP `controlling_office_id`, DROP `controlling_office_name_en`, DROP `controlling_office_name_bn`, DROP `parent_office_id`, DROP `parent_office_name_en`, DROP `parent_office_name_bn`;
