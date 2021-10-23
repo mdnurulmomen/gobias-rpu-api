60ebc8d329b62d61657a08132e60d3567f8cb658
Robin <faisalrobin22@gmail.com>
2021-10-23 20:12:16 +0600

    type name

--- app/Services/AcMemoService.php
+++ app/Services/AcMemoService.php
@@ -17,10 +17,11 @@ class AcMemoService
                 $memos = $request->memos;
 
                 foreach ($memos as $key => $memo){
+                    $memo_attachment = json_encode($memo['ac_memo_attachments']);
                     $ac_memo = new AcMemo;
                     $ac_memo->onucched_no = $memo['onucched_no'];
-                    $ac_memo->memo_irregularity_type = $memo['memo_irregularity_type'];
-                    $ac_memo->memo_irregularity_sub_type = $memo['memo_irregularity_sub_type'];
+                    $ac_memo->memo_irregularity_type = $memo['memo_irregularity_type_name'];
+                    $ac_memo->memo_irregularity_sub_type = $memo['memo_irregularity_sub_type_name'];
                     $ac_memo->team_id = $memo['team_id'];
                     $ac_memo->cost_center_id = $memo['cost_center_id'];
                     $ac_memo->cost_center_name_bn = $memo['cost_center_name_bn'];
@@ -30,17 +31,19 @@ class AcMemoService
                     $ac_memo->audit_year_start = $memo['audit_year_start'];
                     $ac_memo->audit_year_end = $memo['audit_year_end'];
                     $ac_memo->ap_office_order_id = $memo['ap_office_order_id'];
+                    $ac_memo->memo_id = $memo['id'];
                     $ac_memo->memo_title_bn = $memo['memo_title_bn'];
                     $ac_memo->audit_type = $memo['audit_type'];
                     $ac_memo->audit_plan_id = $memo['audit_plan_id'];
                     $ac_memo->memo_description_bn = $memo['memo_description_bn'];
-                    $ac_memo->memo_type = $memo['memo_type'];
-                    $ac_memo->memo_status = $memo['memo_status'];
+                    $ac_memo->memo_type = $memo['memo_type_name'];
+                    $ac_memo->memo_status = $memo['memo_status_name'];
                     $ac_memo->memo_send_date = $request->memo_send_date;
                     $ac_memo->audit_conclusion = $memo['audit_conclusion'];
                     $ac_memo->audit_recommendation = $memo['audit_recommendation'];
                     $ac_memo->jorito_ortho_poriman = $memo['jorito_ortho_poriman'];
                     $ac_memo->status = 1;
+                    $ac_memo->memo_attachments = $memo_attachment;
                     $ac_memo->sender_officer_id = $request->sender_officer_id;
                     $ac_memo->sender_officer_name_bn = $request->sender_officer_name_bn;
                     $ac_memo->sender_officer_name_en = $request->sender_officer_name_en;
--- database/alter/alters.sql
+++ database/alter/alters.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `audit_queries` DROP `ministry_id`, DROP `controlling_office_id`, DROP `controlling_office_name_en`, DROP `controlling_office_name_bn`, DROP `parent_office_id`, DROP `parent_office_name_en`, DROP `parent_office_name_bn`;
+ALTER TABLE `ac_memos` ADD `memo_id` INT NOT NULL AFTER `team_id`;
+ALTER TABLE `ac_memos` ADD `memo_attachments` LONGTEXT NULL AFTER `comment`;
