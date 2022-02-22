9651ec27d00d3742540f34648b7d2c2718bccb2e
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2022-02-22 15:34:36 +0600

    memo

--- app/Services/AcMemoService.php
+++ app/Services/AcMemoService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use App\Traits\SendNotification;
 use App\Models\AcMemo;
@@ -14,52 +15,65 @@ class AcMemoService
     {
         try {
 
-                $memos = $request->memos;
+            $memo = $request->memo;
+            $memo_attachment = json_encode($memo['ac_memo_attachments']);
 
-                foreach ($memos as $key => $memo){
-                    $memo_attachment = json_encode($memo['ac_memo_attachments']);
-                    $ac_memo = new AcMemo;
-                    $ac_memo->directorate_id = $request->directorate_id;
-                    $ac_memo->directorate_en = $request->directorate_en;
-                    $ac_memo->directorate_bn = $request->directorate_bn;
-                    $ac_memo->onucched_no = $memo['onucched_no'];
-                    $ac_memo->memo_irregularity_type = $memo['memo_irregularity_type_name'];
-                    $ac_memo->memo_irregularity_sub_type = $memo['memo_irregularity_sub_type_name'];
-                    $ac_memo->team_id = $memo['team_id'];
-                    $ac_memo->cost_center_id = $memo['cost_center_id'];
-                    $ac_memo->cost_center_name_bn = $memo['cost_center_name_bn'];
-                    $ac_memo->cost_center_name_en = $memo['cost_center_name_en'];
-                    $ac_memo->fiscal_year_id = $memo['fiscal_year_id'];
-                    $ac_memo->fiscal_year = $memo['fiscal_year'];
-                    $ac_memo->ac_query_potro_no = $memo['ac_query_potro_no'];
-                    $ac_memo->audit_year_start = $memo['audit_year_start'];
-                    $ac_memo->audit_year_end = $memo['audit_year_end'];
-                    $ac_memo->ap_office_order_id = $memo['ap_office_order_id'];
-                    $ac_memo->memo_id = $memo['id'];
-                    $ac_memo->memo_title_bn = $memo['memo_title_bn'];
-                    $ac_memo->audit_type = $memo['audit_type'];
-                    $ac_memo->audit_plan_id = $memo['audit_plan_id'];
-                    $ac_memo->memo_description_bn = $memo['memo_description_bn'];
-                    $ac_memo->memo_type = $memo['memo_type_name'];
-                    $ac_memo->memo_status = $memo['memo_status_name'];
-                    $ac_memo->memo_send_date = $request->memo_send_date;
-                    $ac_memo->response_of_rpu = $memo['response_of_rpu'];
-                    $ac_memo->audit_conclusion = $memo['audit_conclusion'];
-                    $ac_memo->audit_recommendation = $memo['audit_recommendation'];
-                    $ac_memo->jorito_ortho_poriman = $memo['jorito_ortho_poriman'];
-                    $ac_memo->status = 1;
-                    $ac_memo->memo_attachments = $memo_attachment;
-                    $ac_memo->sender_officer_id = $request->sender_officer_id;
-                    $ac_memo->sender_officer_name_bn = $request->sender_officer_name_bn;
-                    $ac_memo->sender_officer_name_en = $request->sender_officer_name_en;
-                    $ac_memo->sender_designation_id = $request->sender_designation_id;
-                    $ac_memo->sender_designation_bn = $request->sender_designation_bn;
-                    $ac_memo->sender_designation_en = $request->sender_designation_en;
-                    $ac_memo->save();
-                }
+            $ac_memo = new AcMemo;
+            $ac_memo->memo_id = $memo['id'];
+            $ac_memo->directorate_id = $request->directorate_id;
+            $ac_memo->directorate_en = $request->directorate_en;
+            $ac_memo->directorate_bn = $request->directorate_bn;
+            $ac_memo->directorate_address = $request->directorate_address;
+            $ac_memo->directorate_website = $request->directorate_website;
+            $ac_memo->onucched_no = $memo['onucched_no'];
+            $ac_memo->memo_irregularity_type = $memo['memo_irregularity_type_name'];
+            $ac_memo->memo_irregularity_sub_type = $memo['memo_irregularity_sub_type_name'];
+            $ac_memo->cost_center_id = $memo['cost_center_id'];
+            $ac_memo->cost_center_name_bn = $memo['cost_center_name_bn'];
+            $ac_memo->cost_center_name_en = $memo['cost_center_name_en'];
+            $ac_memo->fiscal_year_id = $memo['fiscal_year_id'];
+            $ac_memo->fiscal_year = $memo['fiscal_year'];
+            $ac_memo->ac_query_potro_no = $memo['ac_query_potro_no'];
+            $ac_memo->audit_year_start = $memo['audit_year_start'];
+            $ac_memo->audit_year_end = $memo['audit_year_end'];
+            $ac_memo->ap_office_order_id = $memo['ap_office_order_id'];
+            $ac_memo->audit_type = $memo['audit_type'];
+            $ac_memo->audit_plan_id = $memo['audit_plan_id'];
 
-                return ['status' => 'success', 'data' => 'Send Successfully'];
+            $ac_memo->team_id = $memo['team_id'];
+            $ac_memo->team_leader_name = $memo['team_leader_name'];
+            $ac_memo->team_leader_designation = $memo['team_leader_designation'];
+            $ac_memo->sub_team_leader_name = $memo['sub_team_leader_name'];
+            $ac_memo->sub_team_leader_designation = $memo['sub_team_leader_designation'];
+            $ac_memo->issued_by = $request->issued_by;
 
+            $ac_memo->memo_title_bn = $memo['memo_title_bn'];
+            $ac_memo->memo_description_bn = $memo['memo_description_bn'];
+            $ac_memo->irregularity_cause = $memo['irregularity_cause'];
+            $ac_memo->memo_type = $memo['memo_type_name'];
+            $ac_memo->memo_status = $memo['memo_status_name'];
+            $ac_memo->memo_send_date = $request->memo_send_date;
+            $ac_memo->jorito_ortho_poriman = $memo['jorito_ortho_poriman'];
+
+            $ac_memo->response_of_rpu = $memo['response_of_rpu'];
+            $ac_memo->audit_conclusion = $memo['audit_conclusion'];
+            $ac_memo->audit_recommendation = $memo['audit_recommendation'];
+
+            $ac_memo->status = 1;
+            $ac_memo->memo_sharok_no = $request->memo_sharok_no;
+            $ac_memo->memo_sharok_date = $request->memo_sharok_date;
+
+            $ac_memo->sender_officer_id = $request->sender_officer_id;
+            $ac_memo->sender_officer_name_bn = $request->sender_officer_name_bn;
+            $ac_memo->sender_officer_name_en = $request->sender_officer_name_en;
+            $ac_memo->sender_designation_id = $request->sender_designation_id;
+            $ac_memo->sender_designation_bn = $request->sender_designation_bn;
+            $ac_memo->sender_designation_en = $request->sender_designation_en;
+            $ac_memo->rpu_acceptor_designation_name_bn = $request->rpu_acceptor_designation_name_bn;
+            $ac_memo->memo_cc = $request->memo_cc;
+            $ac_memo->memo_attachments = $memo_attachment;
+            $ac_memo->save();
+            return ['status' => 'success', 'data' => 'Send Successfully'];
         } catch (\Exception $exception) {
             return ['status' => 'error', 'data' => $exception->getMessage()];
         }
