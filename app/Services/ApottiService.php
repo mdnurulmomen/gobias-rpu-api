55b8af01003507e1dd934eeef6191b0f39f1e3d9
Robin <faisalrobin22@gmail.com>
2022-06-08 16:24:47 +0600

    sender type in braodsheet

--- app/Services/ApottiService.php
+++ app/Services/ApottiService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\BroadSheetReply;
 use App\Models\BroadSheetReplyItem;
+use App\Models\Office;
 use Illuminate\Http\Request;
 use App\Models\ApottiItem;
 
@@ -87,6 +88,24 @@ class ApottiService
                     return $q->where('file_type', 'broadsheet');
                 })->get();
 
+            if($request->sender_type == 'ministry'){
+
+                $ministry_office_info =   Office::select('id','office_name_bng','office_name_eng')
+                    ->where('office_ministry_id',$request->ministry_id)
+                    ->where('is_ministry',1)
+                    ->first();
+
+                $sender_office_id = $ministry_office_info->id;
+                $sender_office_name_bn = $ministry_office_info->office_name_bng;
+                $sender_office_name_en = $ministry_office_info->office_name_eng;
+
+            }elseif($request->sender_type == 'entity'){
+                $sender_office_id = $request->entity_id;
+                $sender_office_name_bn = $request->entity_bn;
+                $sender_office_name_en = $request->entity_en;
+            }
+
+
             //for broadsheet reply
             $broadSheetReply =  new BroadSheetReply();
             $broadSheetReply->directorate_id = $request->directorate_id;
@@ -99,9 +118,9 @@ class ApottiService
 
             $broadSheetReply->memorandum_no = $request->memorandum_no;
             $broadSheetReply->memorandum_date = date('Y-m-d',strtotime($request->memorandum_date));
-            $broadSheetReply->sender_office_id = $request->sender_type == 'ministry' ? $request->ministry_id : $request->entity_id;
-            $broadSheetReply->sender_office_name_bn = $request->sender_type == 'ministry' ? $request->ministry_bn : $request->entity_bn;
-            $broadSheetReply->sender_office_name_en = $request->sender_type == 'ministry' ? $request->ministry_en : $request->entity_en;;
+            $broadSheetReply->sender_office_id = $sender_office_id;
+            $broadSheetReply->sender_office_name_bn = $sender_office_name_bn;
+            $broadSheetReply->sender_office_name_en = $sender_office_name_bn;
             $broadSheetReply->sender_name_bn = $request->sender_name_bn;
             $broadSheetReply->sender_name_en = $request->sender_name_bn;
             $broadSheetReply->sender_type = $request->sender_type;
@@ -111,6 +130,7 @@ class ApottiService
             $broadSheetReply->subject = $request->subject;
             $broadSheetReply->details = $request->details;
             $broadSheetReply->cc_list = $request->cc_list;
+            $broadSheetReply->broad_sheet_type = $request->broad_sheet_type;
             $broadSheetReply->save();
 
             foreach ($apottiItems as $apottiItem){
@@ -133,21 +153,20 @@ class ApottiService
             $data['ministry_name_bn'] = $request->ministry_bn;
             $data['memorandum_no'] = $request->memorandum_no;
             $data['memorandum_date'] = $request->memorandum_date;
-            $data['sender_office_id'] = 1;
-            $data['sender_office_name_bn'] = 'safdsf';
-            $data['sender_office_name_en'] = 'sdfasd';
-            $data['sender_name_bn'] = 'sdfdf';
-            $data['sender_name_en'] = 'dsfsdfds';
-            $data['sender_designation_bn'] = 'sdfds';
-            $data['sender_designation_en'] = 'sdfsd';
-            $data['sender_type'] = 'entity';
+            $data['sender_office_id'] = $sender_office_id;
+            $data['sender_office_name_bn'] = $sender_office_name_bn;
+            $data['sender_office_name_en'] = $sender_office_name_en;
+            $data['sender_name_bn'] = $request->sender_name_bn;
+            $data['sender_name_en'] = $request->sender_name_en;
+            $data['sender_designation_bn'] = $request->sender_designation_bn;
+            $data['sender_designation_en'] = $request->sender_designation_en;
+            $data['sender_type'] = $request->sender_type;
             $data['receiver_details'] = $request->receiver_details;
             $data['subject'] = $request->subject;
             $data['details'] = $request->details;
             $data['cc_list'] = $request->cc_list;
             $data['broad_sheet_type'] = $request->broad_sheet_type;
-            //$send_response = $this->initBeeHttp()->post(config('cag_amms_api.send_apotti_reply'), $data)->json();
-            //dd($send_response);
+
             return ['status' => 'success', 'data' => $data];
 
         } catch (\Exception $exception) {
