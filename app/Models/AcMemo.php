49ffbdbb2b862920a136aebaa84ba97e21206e32
Robin <faisalrobin22@gmail.com>
2022-07-02 15:08:27 +0600

    Apotti soft delete

--- app/Models/AcMemo.php
+++ app/Models/AcMemo.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AcMemo extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $fillable = [
         'memo_id',
